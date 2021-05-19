7656a9b0c3784fca3904461733eaca2e4862bf63
Jerry Nguyen <coh.jr11@gmail.com>
2021-05-19 15:14:51 -0700

    server covid tracker

---
+++ server/index.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const route = require("./routes/route");
+
+const app = express();
+app.use(express.urlencoded({ extended: "false" }));
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/", route);
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
