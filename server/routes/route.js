b8f6a797ce213cb20746373ff634b2fb7b5675af
Jerry Nguyen <coh.jr11@gmail.com>
2021-05-19 20:59:23 -0700

    server covid tracker fix get /getCountries

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const router = express.Router();
+const axios = require("axios");
 
 router.get("/", async (req, res) => {
   res.send("done");
+});
+router.get("/getCountries", (req, res) => {
+  axios
+    .get("https://countryapi.gear.host/v1/Country/getCountries")
+    .then((response) => {
+      res.json({ success: true, data: response.data });
+    });
 });
 
 module.exports = router;
